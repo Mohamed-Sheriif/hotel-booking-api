710f20d8beaeaf4012b0ae8a17a6c953271574d5
Mohamed-Sheriif <mohamedsh2782@gmail.com>
2025-09-28 12:23:38 +0300

    fix payment bug , jwt service want imported in payment module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -9,10 +9,15 @@ import { ReservationsModule } from '../reservations/reservations.module';
 import { StripeModule } from '../stripe/stripe.module';
 import { AuthorizeGuard } from '../auth/guards/authorize.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import authConfig from 'src/auth/config/auth.config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Payment]),
+    ConfigModule.forFeature(authConfig),
+    JwtModule.registerAsync(authConfig.asProvider()),
     ReservationsModule,
     StripeModule,
   ],
